0687ca6e9ed3774271ed609e106fc891c4fbedea
Mohammad Shabana <MShbana@pm.me>
2021-04-01 01:38:33 +0200

    Update heroku app URL in allowed hosts

--- promo_system/settings.py
+++ promo_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
-    'https://promo-system.herokuapp.com/',
+    'promo-system.herokuapp.com',
     'localhost',
     '127.0.0.1'
 
