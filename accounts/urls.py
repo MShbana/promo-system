786f8646f4103a06ea7686f1f74e919efe33b701
Mohammad Shabana <MShbana@pm.me>
2021-03-31 17:22:59 +0200

    Create basic login URL (one URL for both types of users)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
 
 app_name = 'accounts'
@@ -15,4 +17,9 @@ urlpatterns = [
         views.RegisterNormalUser.as_view(),
         name='register_normal_user',
     ),
+    path(
+        'login/',
+        obtain_auth_token,
+        name='login',
+    )
 ]
